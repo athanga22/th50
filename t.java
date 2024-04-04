32f4e51f2e925875ad4db194455d3cc638b37b2f
arsule375 <95156542+arsule375@users.noreply.github.com>
2024-04-04 15:37:47 -0700

    Add files via upload

--- t.java
+++ t.java
@@ -3,7 +3,7 @@ package try5;
 public class t { 
 	
 	public static void main(String[] args) {
-    System.out.println("Hello! TH50");
+    System.out.println("Hello! TH50 My name is atharv Sule ");
 }
 	
 	
